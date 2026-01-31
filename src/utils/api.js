d64357761d7ea66208c492606b406ea888c141ad
Luckyv88 <126507139+Luckyv88@users.noreply.github.com>
2026-02-01 00:49:51 +0530

    add localhost url to render url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://presence-f0s5.onrender.com",
   withCredentials: true, // send cookies for JWT auth
 });
