1bf4768612f72364c827a1e21f8d67cd95337aff
Luckyv88 <126507139+Luckyv88@users.noreply.github.com>
2026-02-01 10:35:47 +0530

    correct the utils/axios url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://presence-f0s5.onrender.com",
+  baseURL: "https://presence-f0s5.onrender.com/api",
   withCredentials: true, // send cookies for JWT auth
 });
