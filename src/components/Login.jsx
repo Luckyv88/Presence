c0387084547eefaa5139ba8fb187d854af19a46f
Luckyv88 <126507139+Luckyv88@users.noreply.github.com>
2026-02-01 17:18:49 +0530

    correct the path of register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -47,7 +47,7 @@ const Login = ({ setUser }) => {
       </form>
 
       <p>
-        Don't have an account? <a href="/signup">Register</a>
+        Don't have an account? <a href="/auth/signup">Register</a>
       </p>
     </div>
   );
