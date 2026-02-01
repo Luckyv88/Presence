83a53571fda609e5032dd3e79f541ac05dcec06f
Luckyv88 <126507139+Luckyv88@users.noreply.github.com>
2026-02-01 16:58:20 +0530

    correct the socket

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,32 +1,41 @@
-const userSocketMap = new Map();
+import { Server } from "socket.io";
 
-io.on("connection", (socket) => {
-
-  socket.on("join", (userId) => {
-    userSocketMap.set(userId, socket.id);
-    socket.join(userId); // 🔥 userId room
+export const initSocket = (httpServer) => {
+  const io = new Server(httpServer, {
+    cors: {
+      origin: [
+        "http://localhost:3000",
+        "https://presences-5a8n.onrender.com"
+      ],
+      credentials: true,
+    },
   });
 
-  socket.on("callUser", ({ to, from, signalData }) => {
-    io.to(to).emit("incomingCall", { from, signalData });
+  io.on("connection", (socket) => {
+    console.log("Socket connected:", socket.id);
+
+    socket.on("join", (userId) => {
+      socket.join(userId);
+      socket.userId = userId;
+      console.log(`User ${userId} joined room`);
+    });
+
+    socket.on("callUser", ({ to, signal, from }) => {
+      io.to(to).emit("incomingCall", { from, signal });
+    });
+
+    socket.on("acceptCall", ({ to, signal }) => {
+      io.to(to).emit("callAccepted", signal);
+    });
+
+    socket.on("endCall", ({ to }) => {
+      io.to(to).emit("callEnded");
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected:", socket.id);
+    });
   });
 
-  socket.on("acceptCall", ({ to, signalData }) => {
-    io.to(to).emit("callAccepted", { signalData });
-  });
-
-  // 🔥 HARD RESET EVENT
-  socket.on("endCall", ({ to, from }) => {
-    io.to(to).emit("callEnded");
-    io.to(from).emit("callEnded");
-  });
-
-  socket.on("disconnect", () => {
-    for (const [uid, sid] of userSocketMap.entries()) {
-      if (sid === socket.id) {
-        userSocketMap.delete(uid);
-        break;
-      }
-    }
-  });
-});
+  return io;
+};
