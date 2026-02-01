713cfcbf65e293127824570fae38145a0e2f5979
Luckyv88 <126507139+Luckyv88@users.noreply.github.com>
2026-02-01 13:36:14 +0530

    correct the socket

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,45 +1,32 @@
-import { Server } from "socket.io";
+const userSocketMap = new Map();
 
-let io;
+io.on("connection", (socket) => {
 
-export const initSocket = (server) => {
-  io = new Server(server, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-    },
+  socket.on("join", (userId) => {
+    userSocketMap.set(userId, socket.id);
+    socket.join(userId); // 🔥 userId room
   });
 
-  io.on("connection", (socket) => {
-    console.log("Socket connected:", socket.id);
-
-    socket.on("join", (userId) => {
-      socket.userId = userId;
-      socket.join(userId);
-      console.log(`User ${userId} joined room`);
-    });
-
-    socket.on("callUser", ({ to, from, signalData, callType }) => {
-      io.to(to).emit("incomingCall", { from, signalData, callType });
-    });
-
-    socket.on("acceptCall", ({ to, signalData }) => {
-      io.to(to).emit("callAccepted", { signalData });
-    });
-
-    socket.on("endCall", ({ to, from }) => {
-      io.to(to).emit("callEnded", { from, to });
-    });
-
-    socket.on("leaveCall", ({ userId }) => {
-      socket.leave(userId);
-      console.log(`User ${userId} left call room`);
-    });
-
-    socket.on("disconnect", () => {
-      console.log("Socket disconnected:", socket.id);
-    });
+  socket.on("callUser", ({ to, from, signalData }) => {
+    io.to(to).emit("incomingCall", { from, signalData });
   });
-};
 
-export const getIO = () => io;
+  socket.on("acceptCall", ({ to, signalData }) => {
+    io.to(to).emit("callAccepted", { signalData });
+  });
+
+  // 🔥 HARD RESET EVENT
+  socket.on("endCall", ({ to, from }) => {
+    io.to(to).emit("callEnded");
+    io.to(from).emit("callEnded");
+  });
+
+  socket.on("disconnect", () => {
+    for (const [uid, sid] of userSocketMap.entries()) {
+      if (sid === socket.id) {
+        userSocketMap.delete(uid);
+        break;
+      }
+    }
+  });
+});
