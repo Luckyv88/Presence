31a94b2d77c4352d9a0d6e5152132e7458b88e82
Luckyv88 <126507139+Luckyv88@users.noreply.github.com>
2026-02-02 12:22:33 +0530

    show status like online

--- src/socket/index.js
+++ src/socket/index.js
@@ -11,37 +11,44 @@ export const initSocket = (httpServer) => {
     },
   });
 
+  const onlineUsers = new Map(); // userId -> socket.id
+
   io.on("connection", (socket) => {
     console.log("Socket connected:", socket.id);
 
     socket.on("join", (userId) => {
       socket.join(userId);
       socket.userId = userId;
+      onlineUsers.set(userId, socket.id);
       console.log(`User ${userId} joined room`);
+
+      // Broadcast user online status
+      io.emit("updateStatus", { userId, status: "online" });
     });
 
-socket.on("callUser", ({ to, signalData, from, callType }) => {
-  io.to(to).emit("incomingCall", {
-    from,
-    signalData,
-    callType
-  });
-});
+    socket.on("callUser", ({ to, signalData, from, callType }) => {
+      io.to(to).emit("incomingCall", { from, signalData, callType });
+      io.emit("updateStatus", { userId: to, status: "ringing" });
+    });
 
-socket.on("acceptCall", ({ to, signalData }) => {
-  io.to(to).emit("callAccepted", { signalData });
-});
-
-    socket.on("acceptCall", ({ to, signal }) => {
-      io.to(to).emit("callAccepted", signal);
+    socket.on("acceptCall", ({ to, signalData }) => {
+      io.to(to).emit("callAccepted", { signalData });
+      io.emit("updateStatus", { userId: to, status: "online" });
     });
 
     socket.on("endCall", ({ to }) => {
       io.to(to).emit("callEnded");
+      io.emit("updateStatus", { userId: to, status: "online" });
     });
 
     socket.on("disconnect", () => {
       console.log("Socket disconnected:", socket.id);
+      const disconnectedUser = [...onlineUsers.entries()].find(([k, v]) => v === socket.id);
+      if (disconnectedUser) {
+        const [userId] = disconnectedUser;
+        onlineUsers.delete(userId);
+        io.emit("updateStatus", { userId, status: "offline" });
+      }
     });
   });
 
